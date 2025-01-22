5fdfa5893d2a2f125d9817661cce3afc4a8415df
Vishnu <166791292+IncarnatorX@users.noreply.github.com>
2025-01-22 07:15:52 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const button = document.querySelector(".btn");
 
+const title = document.querySelector("h1");
+
 button.addEventListener("click", ranNumGen);
 
 function ranNumGen() {
-  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
   document
     .querySelector(".img1")
@@ -16,13 +18,12 @@ function ranNumGen() {
     .setAttribute("src", `./images/dice${randomNumber2}.png`);
 
   if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 1 wins";
+    title.innerHTML = "Player 1 wins";
   } else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 2 wins";
+    title.innerHTML = "Player 2 wins";
   } else {
-    document.querySelector("h1").innerHTML = "It's a Draw!";
+    title.innerHTML = "It's a Draw!";
   }
 
-  document.querySelector(".btn").innerHTML =
-    "Keep clicking me to continue rolling the dice";
+  button.innerHTML = "Keep clicking me to continue rolling the dice";
 }
